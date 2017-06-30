a73e6dad5c833034639cb4a979e175b1c5e08795
Aaron Cameresi <aaroncameresi@gmail.com>
2017-06-30 16:16:40 -0400

    update to pass numbers

--- division.rb
+++ division.rb
@@ -3,18 +3,26 @@
 
 #division
 
-def division
-		print "enter an integer.\n"
-			a = gets.to_i
+# def division
+# 		print "enter an integer.\n"
+# 			a = gets.to_i
 			
-		print "enter an integer to divide by.\n"
-			b = gets.to_i
+# 		print "enter an integer to divide by.\n"
+# 			b = gets.to_i
 
-				if b == 0 #divide by zero exception
-					print "cannot divide by zero.\n"
-				else
-					c = a / b 
-				end
+# 				if b == 0 #divide by zero exception
+# 					print "cannot divide by zero.\n"
+# 				else
+# 					c = a / b 
+# 				end
+# end
+
+# division
+
+
+def division(number1, number2)
+	total = number1 / number2
+	p total
 end
 
-division
+division(8, 4)
